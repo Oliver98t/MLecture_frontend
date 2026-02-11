1dfdb1a0c126ea4476ca0c7f21c43e704efe29f7
Oliver Tattersfield <ot153954@renishaw.com>
2026-02-11 13:47:26 +0000

    added test config to

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Login from "@/pages/login";
 function App() {
   return (
     <Routes>
-      <Route element={<Login />} path="/login" />
+      <Route element={<Login />} path="/" />
       <Route element={<MainPage />} path="/main" />
     </Routes>
   );
