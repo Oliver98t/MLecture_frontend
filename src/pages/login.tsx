57b6e265560b43d60519101c0e73c2ae8e51ac58
Oliver Tattersfield <ot153954@renishaw.com>
2026-02-11 11:00:11 +0000

    Corrected  build error

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,35 +1,16 @@
-import { APIData } from "./APIData";
-import { Input } from "@heroui/input";
 import { Button } from "@heroui/button";
 import { button as buttonStyles } from "@heroui/theme";
-import { useState } from "react";
+
 import DefaultLayout from "@/layouts/default";
 import "katex/dist/katex.min.css";
 
 export default function Login() {
-    const [email, setEmail] = useState<string>(""); // 1. State for input
-    const [password, setPassword] = useState<string>("");
-    var apiData = new APIData();
-
-    const createNotesHandler = async () => {
-        console.log(`${email} ${password}`);
-        await apiData.loginUser(email, password);
-    };
 
     return (
         <DefaultLayout>
             <section className="flex flex-col items-center justify-center min-h-screen gap-4">
                 <div className="w-full max-w-lg mx-auto flex gap-2 items-end justify-center">
-                    <   Input label="Email"
-                        placeholder="Enter your email"
-                        type="email"
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                    <   Input label="Password"
-                        placeholder="Enter your password"
-                        type="password"
-                        onChange={e => setPassword(e.target.value)}
-                    />
+
                     <Button
                         className={buttonStyles({
                             color: "primary",
